30d30e393cebd955703f4775e1bdcd54c46c934e
Mehul Gupta <mehulsvps000@gmail.com>
2022-03-05 20:14:35 +0530

    Basic Components added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const date = new Date();
+const year = date.getFullYear();
+
+function Footer(){
+
+    return <div>
+        <footer>
+            <p>
+                copyright @{year}
+            </p>
+        </footer>
+    </div>
+
+}
+
+export default Footer;
